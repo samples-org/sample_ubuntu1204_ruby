0b7e071b9658c3a09bf3f94a2ff107ccfe428058
csharp <shipci@outlook.com>
2015-04-03 13:05:30 +0530

    Update bowling_spec.rb

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -4,7 +4,7 @@ require './bowling'
 describe Bowling, "#score" do
   it "returns 0 for all gutter game" do
     bowling = Bowling.new
-    20.times { bowling.hit(1) }
-    bowling.score.should eq(1)
+    20.times { bowling.hit(0) }
+    bowling.score.should eq(0)
   end
 end
